03e0704e981d53abed14ea3cdd928a1fb1b5e274
Caroline <2387391668@qq.com>
2018-02-07 20:53:57 +0800

    添加保存为图片和打印PDF功能

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,8 +3,8 @@
   <div id="topbar" class="topbar-wrapper">
     <h2 class="title">{{title}}</h2>
     <div class="actions">
-      <a href="javascript:;">保存图片</a>
-      <a href="javascript:;">保存PDF</a>
+      <a href="javascript:;" @click="saveTo('img')">保存图片</a>
+      <a href="javascript:;" @click="saveTo('pdf')">保存PDF</a>
     </div>
   </div>
 
@@ -12,11 +12,37 @@
 
 <script>
 
+  import html2canvas from 'html2canvas'
+  import jsPDF from 'jsPDF'
+
   export default{
     name:'Topbar',
     data(){
         return {
           title:"Resume",
+        }
+    },
+    methods: {
+        saveTo(type){
+            var printArea = document.getElementById('resumeEdit')
+            //通过html2canvas把html渲染成canvas，然后获取图片数据
+            html2canvas(printArea).then(function (canvas) {
+                //canvas图片转成dataurl
+                var imageData = canvas.toDataURL()
+                type === 'img' ? this.saveImg(imageData) : this.savePdf(imageData, canvas)
+            }.bind(this))
+        },
+        saveImg(dataUrl){
+            var aElement = document.createElement('a');
+            aElement.setAttribute('href', dataUrl);
+            aElement.setAttribute('download', 'resume.png');
+            document.body.insertAdjacentElement('beforeend', aElement)
+            aElement.click();
+        },
+        savePdf(dataUrl, canvas){
+            var doc = new jsPDF('', 'pt', 'a4')
+            doc.addImage(dataUrl, 'png', -10, 0, 595.28, 595.28 / canvas.width * canvas.height)
+            doc.save('resume.pdf')
         }
     }
   }
