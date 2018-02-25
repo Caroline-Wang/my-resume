56b5499f45ed7ae9a2d1c395347f192e09ddfb8f
Caroline <2387391668@qq.com>
2018-02-25 13:59:44 +0800

    修复打包后图片无法加载的问题

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/my-resume/dist/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
